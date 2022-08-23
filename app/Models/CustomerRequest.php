602613d825629c8e482ebf1fbd70e2a0903f15e4
EchoZahar <echo.zahar@gmail.com>
2022-08-23 18:55:45 +0300

    start a task. Add CustomerRequest model and migration. Add emails log file.

---
+++ app/Models/CustomerRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerRequest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'email', 'status', 'message', 'comment'
+    ];
+
+    const ACTIVE = 'Active';
+    const RESOLVED = 'Resolved';
+    public static $status = [self::ACTIVE, self::RESOLVED];
+}
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\Facades\Gate as Gate;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+         'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot(GateContract $gate)
+    {
+        $this->registerPolicies($gate);
+
+        $gate->define('isAdmin', fn ($user) => $user->type === 'admin');
+
+        $gate->define('isCustomer', fn ($user) => $user->type === 'customer');
+    }
+}
